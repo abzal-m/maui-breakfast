73b6df1816a5345379708e2944a4b9fe6c741e60
AbzalMautaliyev <abzalmautaliev@gmail.com>
2022-12-29 01:15:55 +0600

    End of main page

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,20 +1,18 @@
-﻿namespace MauiBreakfast;
+﻿using CommunityToolkit.Maui;
 
+namespace MauiBreakfast;
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
-				fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
-			});
-
-		return builder.Build();
-	}
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+            fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+        }).UseMauiCommunityToolkit();
+        return builder.Build();
+    }
 }
